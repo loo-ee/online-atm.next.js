7708b6ab2deadcc97ff10082e5921c116af0e875
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-31 16:06:41 +0800

    Add a type definition file

---
+++ src/util/types.d.ts
@@ -0,0 +1,45 @@
+export const backendUrl = 'https://goldfish-app-3tklk.ondigitalocean.app/';
+
+export type BankModel = {
+  bankName: string;
+  description: string;
+  thumbnail: string;
+};
+
+export type UserModel = {
+  email: string;
+  password: string;
+  accounts: AccountModel[];
+  avatar: string | null;
+  lastLogin: string | null;
+  username: string;
+  isAdmin: boolean;
+};
+
+export type AccountModel = {
+  bank: string;
+  name: string;
+  accountNumber: number;
+  pin: number;
+  balance: number;
+};
+
+export type AccountRequest = {
+  username: string;
+  userEmail: string;
+  bank: string;
+};
+
+export type ChangePinRequest = {
+  accountNumber: number;
+  newPin: number;
+  title: string;
+  body: string;
+};
+
+export type MessageModel = {
+  sender: string;
+  receiver: string;
+  title: string;
+  body: string;
+};
