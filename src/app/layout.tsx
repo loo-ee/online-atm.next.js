b184787f6f08f88b004ab7d89c1d79fdba8daf9c
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-31 23:08:42 +0800

    Use Rubik font for project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,20 @@ import UserContextProvider from '@/contexts/UserContext';
 import './globals.css';
 import Header from './Header';
 
+import { Rubik } from '@next/font/google';
+
+const rubik = Rubik({
+  subsets: ['latin'],
+  display: 'optional',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head />
+    <html lang="en" className={rubik.className}>
       <body className="flex flex-col bg-primary">
         <UserContextProvider>
           <Header />
