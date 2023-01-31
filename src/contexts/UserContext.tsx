393b5a434e98915809535668a43de9c93c0ea357
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-31 16:26:09 +0800

    Add UserContextProvider

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { UserModel } from '@/util/types';
+import React, { useState } from 'react';
+
+export interface UserContextProp {
+  user: UserModel;
+  setUser: React.Dispatch<React.SetStateAction<UserModel>>;
+}
+
+export const UserContext = React.createContext<UserContextProp | null>(null);
+
+export default function UserContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const userAcc: UserModel = {
+    username: '???',
+    password: '???',
+    email: '???',
+    avatar: null,
+    lastLogin: null,
+    isAdmin: false,
+    accounts: [],
+  };
+
+  const [userAccount, setUserAccount] = useState<UserModel>(userAcc);
+
+  const User: UserContextProp = {
+    user: userAccount,
+    setUser: setUserAccount,
+  };
+
+  return <UserContext.Provider value={User}>{children}</UserContext.Provider>;
+}
