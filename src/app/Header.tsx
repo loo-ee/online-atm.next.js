c9c6e620f03b2306b49e84404e20fd32a1bef627
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-01-31 15:43:03 +0800

    Customize Header component

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header({}) {
@@ -7,32 +8,35 @@ export default function Header({}) {
       className="flex flex-row justify-between border-b-4 border-black items-center pb-3 w-full fixed bg-secondary z-50"
     >
       <div className="flex flex-row w-full items-center">
-        {/* <img
-          src={
-            new URL('../../assets/images/money-transfer.png', import.meta.url)
-              .href
-          }
-          className="w-16 ml-3 mt-3"
-          onClick={returnToHomePage}
-          alt=""
-        /> */}
-        <Link href="/">
+        <Link href="/" className="flex flex-row ml-3 mt-3">
+          <Image
+            src="/images/money-transfer.png"
+            alt="money transfer image"
+            width={60}
+            height={60}
+          />
+
           <span className="text-u_darkblue ml-5 laptop:text-5xl tablet:text-2xl phone:text-md">
             ATM Service
           </span>
         </Link>
       </div>
       <div className="flex flex-row justify-between phone:w-20 laptop:w-72 mx-7">
-        {/* <img
-          src={new URL('../../assets/images/protect.png', import.meta.url).href}
-          className="w-16 laptop:flex phone:hidden"
-          alt=""
+        <Image
+          src="/images/protect.png"
+          alt="money protection"
+          width={60}
+          height={60}
+          className="laptop:flex phone:hidden"
         />
-        <img
-          src={new URL('../../assets/images/account.png', import.meta.url).href}
-          className="phone:w-10 laptop:w-16 rounded-full ml-2 mt-3 phone:mx-12"
-          alt=""
-        /> */}
+
+        <Image
+          src="/images/account.png"
+          alt="money protection"
+          width={60}
+          height={60}
+          className="rounded-full ml-2 mt-3 phone:mx-12"
+        />
       </div>
     </div>
   );
