43721d72cdbba068cc336ae31ca5f15d27e21a5a
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-02-01 19:15:55 +0800

    Create nullBank-BankModel in globalVars

--- src/util/globalVars.ts
+++ src/util/globalVars.ts
@@ -1,1 +1,10 @@
+import { BankModel } from './types';
+
 export const backendUrl = 'https://goldfish-app-3tklk.ondigitalocean.app';
+
+export const nullBank: BankModel = {
+  id: -1,
+  bankName: 'null',
+  description: 'null',
+  thumbnail: '/images/null.png',
+};
