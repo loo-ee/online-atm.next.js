c80a463a156e5892d5196c62272ab06add34c173
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-02-05 15:50:07 +0800

    Make Panel component fetch all account counts

--- src/app/user/Panel.tsx
+++ src/app/user/Panel.tsx
@@ -1,34 +1,53 @@
 'use client';
 
+import { getAllAccounts } from '@/adapters/userAdapter';
 import { UserContext } from '@/contexts/UserContext';
+import { AccountModel } from '@/util/types';
 import Image from 'next/image';
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 export default function Panel({}) {
   const User = useContext(UserContext);
-  const BDOAccountsCount = useRef(0);
-  const BPIAccountsCount = useRef(0);
-  const LBAccountsCount = useRef(0);
 
-  function getAccountsCount(): void {
-    // User?.user.accounts.map((account) => {
-    //   switch (account.bank) {
-    //     case 'BDO':
-    //       BDOAccountsCount.current++;
-    //       break;
-    //     case 'BPI':
-    //       BPIAccountsCount.current++;
-    //       break;
-    //     case 'LANDBANK':
-    //       LBAccountsCount.current++;
-    //       break;
-    //   }
-    // });
+  const [BDO_count, set_BDO_count] = useState(0);
+  const [BPI_count, set_BPI_count] = useState(0);
+  const [LANDBANK_count, set_LANDBANK_count] = useState(0);
+
+  function getAccountsCount(accounts: AccountModel[]): void {
+    let bdo = 0;
+    let bpi = 0;
+    let landbank = 0;
+
+    accounts.forEach((account) => {
+      switch (account.bank) {
+        case 'BDO':
+          bdo++;
+          break;
+        case 'BPI':
+          bpi++;
+          break;
+        case 'LANDBANK':
+          landbank++;
+          break;
+      }
+    });
+
+    set_BDO_count(bdo);
+    set_BPI_count(bpi);
+    set_LANDBANK_count(landbank);
+  }
+
+  async function fetchAllAcounts() {
+    const foundAccounts = await getAllAccounts(User!.user.email);
+
+    if (foundAccounts) {
+      getAccountsCount(foundAccounts);
+    }
   }
 
   useEffect(() => {
-    getAccountsCount();
-  });
+    fetchAllAcounts();
+  }, []);
 
   return (
     <div className="laptop:p-5 bg-secondary border-4 border-black rounded-lg p-4 laptop:w-[300px] laptop:h-[300px] mt-8">
@@ -48,13 +67,13 @@ export default function Panel({}) {
 
       <div className="text-xs flex flex-col text-start text-white laptop:mt-2">
         <div className="bg-blue-900 p-2 rounded mt-2">
-          You have {BDOAccountsCount.current} BDO accounts.
+          You have {BDO_count} BDO accounts.
         </div>
         <div className="bg-red-900 p-2 rounded mt-2">
-          You have {BPIAccountsCount.current} BPI accounts.
+          You have {BPI_count} BPI accounts.
         </div>
         <div className="bg-green-700 p-2 rounded mt-2">
-          You have {LBAccountsCount.current} LANDBANK accounts.
+          You have {LANDBANK_count} LANDBANK accounts.
         </div>
       </div>
     </div>
