152219b1ab0f272373578a74e7d3620c31863e53
Jann Louie Almirante <92241727+lui523202@users.noreply.github.com>
2023-02-08 23:37:54 +0800

    Change backend url

--- src/util/globalVars.ts
+++ src/util/globalVars.ts
@@ -1,20 +1,20 @@
-import { AccountModel, BankModel } from './types';
+import { AccountModel, BankModel } from "./types";
 
-// export const backendUrl = 'https://goldfish-app-3tklk.ondigitalocean.app';
+export const backendUrl = "https://goldfish-app-3tklk.ondigitalocean.app";
 
-export const backendUrl = 'http://127.0.0.1:8000';
+// export const backendUrl = 'http://127.0.0.1:8000';
 
 export const nullBank: BankModel = {
   id: -1,
-  bankName: 'null',
-  description: 'null',
-  thumbnail: '/images/null.png',
+  bankName: "null",
+  description: "null",
+  thumbnail: "/images/null.png",
 };
 
 export const nullAccount: AccountModel = {
   accountNumber: -1,
   balance: -1,
-  bank: 'null',
-  name: 'null',
+  bank: "null",
+  name: "null",
   pin: -1,
 };
